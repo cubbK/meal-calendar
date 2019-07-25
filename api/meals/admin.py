e3ca345513dca81cfe6f5598529d3433a80de8d4
Dan <danpopa1998@gmail.com>
2019-07-25 03:47:04 +0300

    add models

--- api/meals/admin.py
+++ api/meals/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from meals.models import Meal, Ingredient
+
+admin.site.register(Meal)
+admin.site.register(Ingredient)
--- api/meals/models.py
+++ api/meals/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=100, default='')
+    quantity = models.CharField(max_length=50, default='')
+
+class Meal(models.Model):
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=200, default='')
+    requiresFruit = models.BooleanField(blank=True, default=False)
+    link = models.TextField(default='')
+    calories = models.IntegerField(default=None)
+    servings = models.IntegerField(default=None)
+
+    BREAKFAST = 'BREAKFAST'
+    GENERIC = 'GENERIC'
+    TYPE_MEAL_CHOICES = [
+        (BREAKFAST, 'Breakfast'),
+        (GENERIC, 'Generic')
+    ]
+    typeMeal = models.CharField(
+        max_length=100, choices=TYPE_MEAL_CHOICES, default=GENERIC)
+    ingredients = models.ManyToManyField(Ingredient)
+
+
