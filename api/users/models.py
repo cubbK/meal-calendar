4de6470c1d42457f05394e5df142d3776b06af8e
Dan <danpopa1998@gmail.com>
2019-07-31 17:35:29 +0300

    fix comment about schema validation

--- api/users/models.py
+++ api/users/models.py
@@ -12,7 +12,7 @@ class UserManager(BaseUserManager):
     def _create_user(self, email, password, **extra_fields):
         """Create and save a User with the given email and password."""
         # raising exception here doesn't do anything for whatever reason.
-        # All validation is done in schema function
+        # All validation is done in the schema
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
